d05901b0aadf11a88febb5282ebbb556e276b58d
Roba <njuguiinirobert@gmail.com>
2021-04-27 02:20:54 +0300

    initial commit

---
+++ app/src/main/java/com/example/to_do/Adapters/TaskRecyclerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.to_do.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.to_do.MainActivity;
+import com.example.to_do.Models.TaskModel;
+import com.example.to_do.R;
+
+import java.util.List;
+
+public class TaskRecyclerAdapter extends RecyclerView.Adapter<TaskRecyclerAdapter.ViewHolder> {
+    private List<TaskModel> list;
+    private MainActivity mainActivity;
+
+    public TaskRecyclerAdapter(MainActivity activity){
+        this.mainActivity=activity;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_row,parent,false);
+        return new ViewHolder(view);
+    }
+
+    public void onBindViewHolder(ViewHolder holder, int position){
+        TaskModel taskModel = list.get(position);
+        holder.task.setText(taskModel.getTask());
+        holder.task.setChecked(toBoolean(taskModel.getStatus()));
+    }
+    public int getItemCount(){
+        return list.size();
+    }
+    public boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+
+        ViewHolder(View view){
+            super(view);
+            task = view.findViewById(R.id.checkbox);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/to_do/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.to_do;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        recyclerView = findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
